93c0e21ede12987add2c8db58fb17ba69ff8a517
Jan Funke <funke@ini.ch>
2015-02-24 18:33:05 +0100

    added option solutionWithBorders

--- io/SolutionWriter.cpp
+++ io/SolutionWriter.cpp
@@ -1,6 +1,11 @@
 #include <vigra/impex.hxx>
 #include <util/Logger.h>
+#include <util/ProgramOptions.h>
 #include "SolutionWriter.h"
+
+util::ProgramOption optionSolutionWithBorders(
+		util::_long_name        = "solutionWithBorders",
+		util::_description_text = "Ensure an at least 1-pixel wide background border between each pair of found regions in the solution image.");
 
 logger::LogChannel solutionwriterlog("solutionwriterlog", "[SolutionWriter] ");
 
@@ -32,6 +37,21 @@ SolutionWriter::write() {
 
 			segmentation(p.x, p.y) = slice->getId();
 		}
+	}
+
+	if (optionSolutionWithBorders) {
+
+		for (unsigned int y = 0; y < segmentation.height() - 1; y++)
+			for (unsigned int x = 0; x < segmentation.width() - 1; x++) {
+
+				float value = segmentation(x, y);
+				float right = segmentation(x+1, y);
+				float down  = segmentation(x, y+1);
+				float diag  = segmentation(x+1, y+1);
+
+				if (value != right || value != down || value != diag)
+					segmentation(x, y) = 0;
+			}
 	}
 
 	vigra::exportImage(vigra::srcImageRange(segmentation), vigra::ImageExportInfo(_filename.c_str()));
